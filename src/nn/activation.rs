8bbc10aac8c6a3f700a3da3b7e8b1e76c42340ba
AceofStades <rahulpbokade@gmail.com>
2025-12-17 23:52:02 +0530

    activation.rs: Add sigmoid function

--- src/nn/activation.rs
+++ src/nn/activation.rs
@@ -1,6 +1,12 @@
 use std::f64::consts;
 
-fn sigmoid(x: f32) -> f64 {
-    let e: f64 = consts::E;
-    1.0 / (1.0 + e.powf(x as f64))
+fn sigmoid(x: f32) -> f32 {
+    let e: f32 = consts::E as f32;
+    1.0 / (1.0 + e.powf(-x))
 }
+
+fn relu(x: f32) -> f32 {
+    if x > 0.0 { x } else { 0.0 as f32 }
+}
+
+fn softmax(x: f32) -> f32 {}
