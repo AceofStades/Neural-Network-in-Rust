f0b29c5f9aabd95872014f52b76a722a159e0cc4
AceofStades <rahulpbokade@gmail.com>
2025-12-20 21:42:19 +0530

    nn: Add tests to activations

--- src/nn/activation.rs
+++ src/nn/activation.rs
@@ -19,3 +19,41 @@ pub fn relu(x: f32) -> f32 {
 pub fn softplus(x: f32) -> f32 {
     (1.0 + E.powf(x)).log(E)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_relu_pos() {
+        let input = 10.0;
+        let expected = 10.0;
+        let result = relu(input);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_relu_neg() {
+        let input = -5.0;
+        let result = relu(input);
+
+        assert_eq!(result, 0.0);
+    }
+
+    #[test]
+    fn test_sigmoid() {
+        let input = 0.0;
+        let result = sigmoid(input);
+
+        assert_eq!(result, 0.5);
+    }
+
+    #[test]
+    fn test_tanh() {
+        let input = 0.0;
+        let result = tanh(input);
+
+        assert_eq!(result, 0.0);
+    }
+}
