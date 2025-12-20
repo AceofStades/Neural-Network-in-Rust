5216de004f083b0e922f0ee93c805afa9923b2d0
AceofStades <rahulpbokade@gmail.com>
2025-12-20 14:22:22 +0530

    nn: Add a few more activation functions

--- src/nn/activation.rs
+++ src/nn/activation.rs
@@ -1,12 +1,21 @@
 use std::f64::consts;
 
-fn sigmoid(x: f32) -> f32 {
-    let e: f32 = consts::E as f32;
-    1.0 / (1.0 + e.powf(-x))
+const E: f32 = consts::E as f32;
+
+pub fn sigmoid(x: f32) -> f32 {
+    1.0 / (1.0 + E.powf(-x))
 }
 
-fn relu(x: f32) -> f32 {
+pub fn tanh(x: f32) -> f32 {
+    2.0 / (1.0 + E.powf(-2.0 * x)) - 1.0
+}
+
+pub fn relu(x: f32) -> f32 {
     if x > 0.0 { x } else { 0.0 as f32 }
 }
 
-fn softmax(x: f32) -> f32 {}
+// pub fn softmax(x: f32) -> f32 {}
+
+pub fn softplus(x: f32) -> f32 {
+    (1.0 + E.powf(x)).log(E)
+}
