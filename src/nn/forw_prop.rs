ac5a744d72593f11a20d7a0103bb71de2e9894d7
AceofStades <rahulpbokade@gmail.com>
2025-12-26 22:13:20 +0530

    nn: Update forward_prop function to be public

--- src/nn/forw_prop.rs
+++ src/nn/forw_prop.rs
@@ -1,5 +1,5 @@
 use ndarray::{Array1, Array2};
 
-fn forward_prop(x: Array1<f32>, w: Array2<f32>, b: Array1<f32>) -> Array1<f32> {
+pub fn forward_prop(x: Array1<f32>, w: Array2<f32>, b: Array1<f32>) -> Array1<f32> {
     x
 }
