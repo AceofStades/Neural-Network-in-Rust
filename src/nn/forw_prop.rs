a59514d1e0f03d43a347c708ded9709c7bc3b664
AceofStades <rahulpbokade@gmail.com>
2025-12-28 19:21:04 +0530

    nn: Minor forward prop change

--- src/nn/forw_prop.rs
+++ src/nn/forw_prop.rs
@@ -1,5 +1,6 @@
+use crate::nn::activation;
 use ndarray::{Array1, Array2};
 
-pub fn forward_prop(x: Array1<f32>, w: Array2<f32>, b: Array1<f32>) -> Array1<f32> {
-    x
+pub fn forward_prop(a_prev: Array1<f32>, w: Array2<f32>, b: Array1<f32>) -> Array1<f32> {
+    let z: Array1<f32> = a_prev.dot(w) + b;
 }
