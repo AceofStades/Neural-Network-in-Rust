9987089acbd8605049a729b36769256d79181687
AceofStades <rahulpbokade@gmail.com>
2026-01-01 20:08:59 +0530

    nn: Merging forw_prop and back_prop into layer.rs

--- src/nn/layer.rs
+++ src/nn/layer.rs
@@ -2,5 +2,6 @@ use crate::nn::activation;
 use ndarray::{Array1, Array2};
 
 pub fn forward_prop(a_prev: Array1<f32>, w: Array2<f32>, b: Array1<f32>) -> Array1<f32> {
-    let z: Array1<f32> = a_prev.dot(w) + b;
+    let z: Array1<f32> = a_prev.dot(w[1]) + b;
+    a_prev
 }
