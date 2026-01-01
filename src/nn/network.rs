fff3682009eac354bc89d7e7b3458bb833de2b79
AceofStades <rahulpbokade@gmail.com>
2026-01-01 22:07:24 +0530

    nn/network: Implement forward prop

--- src/nn/network.rs
+++ src/nn/network.rs
@@ -1,6 +1,7 @@
+use crate::nn::activation::*;
 use ndarray::{Array1, Array2};
 
-enum ActivationType {
+pub enum ActivationType {
     Sigmoid,
     ReLU,
     Tanh,
@@ -11,4 +12,16 @@ pub struct Layer {
     pub weights: Array2<f32>,
     pub biases: Array1<f32>,
     pub activation: ActivationType,
+}
+
+impl Layer {
+    pub fn forward(&self, input: &Array1<f32>) -> Array1<f32> {
+        let z = input.dot(&self.weights) + &self.biases;
+        match self.activation {
+            ActivationType::ReLU => z.mapv(|x| relu(x)),
+            ActivationType::Sigmoid => z.mapv(|x| sigmoid(x)),
+            ActivationType::SoftPlus => z.mapv(|x| softplus(x)),
+            ActivationType::Tanh => z.mapv(|x| tanh(x)),
+        }
+    }
 }
