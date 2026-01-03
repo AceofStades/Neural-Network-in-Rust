2b5f7deaf1035757bfa8c028ba50d76be1d4af30
AceofStades <rahulpbokade@gmail.com>
2026-01-03 12:37:42 +0530

    nn/network: Add input and z to struct layer, Initialize back prop

--- src/nn/network.rs
+++ src/nn/network.rs
@@ -1,5 +1,5 @@
 use crate::nn::activation::*;
-use ndarray::{Array1, Array2};
+use ndarray::{Array, Array1, Array2};
 
 pub enum ActivationType {
     Sigmoid,
@@ -12,11 +12,18 @@ pub struct Layer {
     pub weights: Array2<f32>,
     pub biases: Array1<f32>,
     pub activation: ActivationType,
+
+    pub last_input: Option<Array1<f32>>,
+    pub last_z: Option<Array1<f32>>,
 }
 
 impl Layer {
-    pub fn forward(&self, input: &Array1<f32>) -> Array1<f32> {
+    pub fn forward(&mut self, input: &Array1<f32>) -> Array1<f32> {
+        self.last_input = Some(input.clone());
+
         let z = input.dot(&self.weights) + &self.biases;
+        self.last_z = Some(z.clone());
+
         match self.activation {
             ActivationType::ReLU => z.mapv(|x| relu(x)),
             ActivationType::Sigmoid => z.mapv(|x| sigmoid(x)),
